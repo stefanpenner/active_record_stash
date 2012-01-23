4d8e75012205e57efc658ab9b0ed0f8d65f4a253
Stefan Penner <stefan.penner@gmail.com>
2012-01-23 16:12:35 -0600

    various bugs have been squashed.

--- lib/active_record_stash.rb
+++ lib/active_record_stash.rb
@@ -6,23 +6,21 @@ module ActiveRecordStash
 
   NO_TARGET_ERROR = "stashing needs a target serialized column. Supply an options hash with a :in key as the last argument (e.g. stash :apple, :in => :greeter)."
 
-  included do
+  included do |klass|
     class_attribute :stashed_attributes
-    self.stashed_attributes = Hash.new([])
-
-    after_initialize :_load_stashed_attributes
-    before_save :_stash_attributes
+    klass.stashed_attributes = Hash.new([])
+    klass.after_initialize :_load_stashed_attributes
+    klass.before_save :_stash_attributes
   end
 
   private
   def _load_stashed_attributes
     stashed_attributes.each_pair do |store_name,methods|
-      store = send(store_name)
 
-      next unless store
+      return unless store = send(store_name)
 
       methods.each do |method|
-        send :"#{method}=", store[method] unless send(method)
+        send :"#{method}=", store[method] if respond_to? method
       end
 
     end
@@ -31,12 +29,12 @@ module ActiveRecordStash
   def _stash_attributes
     stashed_attributes.each_pair do |store_name,methods|
 
-      eval "self.#{store_name} ||= {}"
-
-      methods.each do |method|
-        send(store_name)[method] = send method
+      data = methods.inject({}) do |data,method|
+        data[method] = send method
+        data
       end
 
+      self[store_name] = data
     end
   end
 
@@ -51,7 +49,7 @@ module ActiveRecordStash
         raise ArgumentError,  NO_TARGET_ERROR
       end
 
-      stashed_attributes[serialized_column] += methods.map(&:to_sym)
+      self.stashed_attributes[serialized_column] += methods.map(&:to_sym)
 
       serialize serialized_column
       attr_accessor *methods
